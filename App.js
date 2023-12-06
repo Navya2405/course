92cce3db91d754769f06fc6a3d5a9189ed75355c
jagadish <pandu516jagu@gmail.com>
2023-12-06 15:07:44 +0530

    my changes

--- App.js
+++ App.js
@@ -4,18 +4,7 @@ import {View, Text, Image} from "react-native";
 export default function App() {
   return (
      <View style ={{ flex: 1, backgroundColor: "plum", padding:60}}> 
-          <Text style={{color:"white"}}> World</Text>
-       <View 
-        style={{ width: 200, height:200, backgroundColor:"lightblue"}}> 
-       </View>
-       <View 
-        style={{ width: 200, height:200, backgroundColor:"white"}}> 
-       </View>
-    
-        <Image 
-          source={require('./assets/adaptive-icon.png')}
-          style={{width:200, height:200}}>
-        </Image>
+          <Text>Hello Navya</Text>
        
      </View>
      
